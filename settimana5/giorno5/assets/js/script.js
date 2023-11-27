fb9a3385b78b4f45fbce5f5114738f0566d21d1a
gabrielepilastri <gabrielepilastri@gmail.com>
2023-11-27 18:56:53 +0100

    commit

--- settimana5/giorno5/assets/js/script.js
+++ settimana5/giorno5/assets/js/script.js
@@ -15,3 +15,30 @@ window.addEventListener("scroll", function(event) {
     }
 })
 
+
+
+function mRandom() {
+
+    let m = document.querySelectorAll("g[stroke-linecap=butt]")
+    let random = Math.floor(Math.random() * m.length);
+    let myM = m[random];
+    return myM;
+};
+
+function mVanish () {
+    let opaca = mRandom()
+    let opacity = parseInt(opaca.getAttribute("opacity"))
+    if (opacity === 0) {
+        opaca.setAttribute("opacity", 1)
+    } else {
+        opaca.setAttribute("opacity", 0)
+    }
+};
+
+function alterna () {
+    setInterval(function () {
+        mVanish()
+    }, 20)
+}
+
+alterna()
