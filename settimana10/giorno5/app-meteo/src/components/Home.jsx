03e776217f738e9a74a6221d3ea98e5079ad9d58
gabrielepilastri <gabrielepilastri@gmail.com>
2024-01-21 09:07:13 +0100

    commit

--- settimana10/giorno5/app-meteo/src/components/Home.jsx
+++ settimana10/giorno5/app-meteo/src/components/Home.jsx
@@ -46,6 +46,7 @@ const Home = ({search}) => {
                 if (res.ok) {
                     let data = await res.json();
                     console.log(data);
+                    console.log(data.list);
                    setCity2(data);
     
                 }else {
@@ -79,7 +80,7 @@ const Home = ({search}) => {
 
                             <div className='center'>
                             <Card.Title className='card-title'>Wind Speed</Card.Title>
-                            <Card.Text>{city?.wind?.speed} km/h</Card.Text>
+                            <Card.Text className='card-text'>{city?.wind?.speed} km/h</Card.Text>
                             </div>
                         </div>
 
@@ -92,7 +93,7 @@ const Home = ({search}) => {
 
                             <div className='center'>
                             <Card.Title className='card-title'>Rain Chanche</Card.Title>
-                            <Card.Text>{city?.clouds?.all} %</Card.Text>
+                            <Card.Text className='card-text'>{city?.clouds?.all} %</Card.Text>
                             </div>
                         </div>
 
@@ -105,7 +106,7 @@ const Home = ({search}) => {
 
                             <div className='center'>
                             <Card.Title className='card-title'>Temperature</Card.Title>
-                            <Card.Text>{city?.main?.temp.toFixed()} °C</Card.Text>
+                            <Card.Text className='card-text'>{city?.main?.temp.toFixed()} °C</Card.Text>
                             </div>
                         </div>
 
@@ -118,7 +119,7 @@ const Home = ({search}) => {
 
                             <div className='center'>
                             <Card.Title className='card-title'>Humidity</Card.Title>
-                            <Card.Text>{city?.main?.humidity} %</Card.Text>
+                            <Card.Text className='card-text'>{city?.main?.humidity} %</Card.Text>
                             </div>
                         </div>
 
@@ -127,11 +128,11 @@ const Home = ({search}) => {
                 <Card className='single-card'>
                     <CardBody>
                         <div className='flex'>
-                            <Card.Img width={40} src='src/assets/percent-solid.svg' />
+                            <Card.Img width={40} src='src/assets/temperature-three-quarters-solid.svg' />
 
                             <div className='center'>
                             <Card.Title className='card-title'>Tomorrow</Card.Title>
-                            {city2 && <Card.Text>{city2?.list[7]?.main?.temp.toFixed()} °C</Card.Text>}
+                            {city2 && <Card.Text className='card-text'>{city2?.list[7]?.main?.temp.toFixed()} °C</Card.Text>}
                             </div>
                         </div>
 
